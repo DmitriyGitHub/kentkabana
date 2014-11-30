6a444bc8521182610d34101936546422424d4bd1
dmitriy <dmitriy.raschepkin@gmail.com>
2014-11-30 18:04:21 +0200

    Vagrant configured

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -3,9 +3,9 @@ return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => 'mysql:host=localhost;dbname=kentkabana',
+            'username' => 'kentkabana',
+            'password' => 'kentkabana',
             'charset' => 'utf8',
         ],
         'mailer' => [
